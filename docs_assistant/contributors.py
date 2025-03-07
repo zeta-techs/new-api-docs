d2ae9654970c7d52e414095794d7c3bc5081253a
Apple\Apple <zeraturing@foxmail.com>
2025-03-07 23:22:30 +0800

    chore: Latest date

--- docs_assistant/contributors.py
+++ docs_assistant/contributors.py
@@ -399,7 +399,7 @@ New API 的开发离不开社区的支持和贡献。在此特别感谢所有为
 
 以下是所有为项目做出贡献的开发者列表。在此感谢他们的辛勤工作和创意！
 
-!!! info "贡献者信息（数据更新于: {current_time}）"
+!!! info "贡献者信息 · 数据更新于 {current_time}）"
     以下贡献者数据从 [GitHub Contributors 页面](https://github.com/Calcium-Ion/new-api/graphs/contributors) 自动获取前50名。贡献度前三名分别以金、银、铜牌边框标识。如果您也想为项目做出贡献，欢迎提交 Pull Request。
 
 {format_contributors_markdown(contributors_data)}
