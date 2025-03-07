fc352bff9d01518a3a661ab5dff01f866b6831ee
Apple\Apple <zeraturing@foxmail.com>
2025-03-07 15:56:55 +0800

    chore: Docs assistant

--- docs_assistant/contributors.py
+++ docs_assistant/contributors.py
@@ -7,7 +7,7 @@ from utils import update_markdown_file, DOCS_DIR
 logger = logging.getLogger('contributors')
 
 def format_contributors_markdown(contributors_data):
-    """将贡献者数据格式化为Markdown内容 - 简化版"""
+    """将贡献者数据格式化为Markdown内容"""
     if not contributors_data or len(contributors_data) == 0:
         return "暂无贡献者数据，请稍后再试。"
     
@@ -162,9 +162,8 @@ New API 的开发离不开社区的支持和贡献。在此特别感谢所有为
 
 以下是所有为项目做出贡献的开发者列表。在此感谢他们的辛勤工作和创意！
 
-!!! info "贡献者信息"
+!!! info "贡献者信息（数据更新于: {current_time}）"
     以下贡献者数据从 [GitHub Contributors 页面](https://github.com/Calcium-Ion/new-api/graphs/contributors) 自动获取前50名。贡献度前三名分别以金、银、铜牌边框标识。如果您也想为项目做出贡献，欢迎提交 Pull Request。
-    数据更新于: {current_time}
 
 """
         contributors_markdown = format_contributors_markdown(contributors_data)
